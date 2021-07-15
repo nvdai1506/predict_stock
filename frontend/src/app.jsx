6c1a4d1a2f6c67f77890aced673fb9ded2a4fd59
Thai Nhat Minh <t.n.minh1997@gmail.com>
2021-07-15 21:22:48 +0700

    + Implement get LSTM model data

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,44 +1,82 @@
-import React from 'react'
-import { Bar } from "react-chartjs-2"
+import React, { Component } from 'react'
+import { Line } from "react-chartjs-2"
 
-const App = () => {
-  return (
-    <h1>
-     Create React App Without CRA ☘️ MNT aaa
-     <Bar
-    data={{
-      labels: [
-        "Africa",
-        "Asia",
-        "Europe",
-        "Latin America",
-        "North America"
-      ],
+const options = {
+  legend: { display: false },
+  title: {
+    display: true,
+    text: "Predicted world population (millions) in 2050"
+  },
+  scales: {
+    xAxes: [{
+      title: "time",
+      type: 'time',
+      gridLines: {
+        lineWidth: 2
+      },
+      time: {
+        unit: 'year'
+      }
+    }]
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: [] }
+    this.chartReference = React.createRef();
+  }
+
+  callApi() {
+    fetch("http://localhost:5000/model?model=LSTM", {
+      method: "GET",
+      crossDomain: true,
+
+    }).then(response => response.json())
+    .then(data => this.updateData(data)).catch(error => console.error(error));
+  }
+
+  updateData(rawData) {
+    this.chartReference.current.data = {
+      labels: [],
       datasets: [
         {
-          label: "Population (millions)",
-          backgroundColor: [
-            "#3e95cd",
-            "#8e5ea2",
-            "#3cba9f",
-            "#e8c3b9",
-            "#c45850"
-          ],
-          data: [2478, 5267, 734, 784, 433]
+          label: "Price",
+          borderColor: 'rgb(75, 192, 192)',
+          data: []
         }
       ]
-    }}
-    options={{
-      legend: { display: false },
-      title: {
-        display: true,
-        text: "Predicted world population (millions) in 2050"
-      }
-    }}
-  />
-    </h1>
-    
-  )
+    };
+
+    var labels = []
+    var data = []
+    rawData.forEach(element => {
+      labels.push(new Date(element[0]));
+      data.push({x: new Date(element[0]), y: element[1]})
+    });
+    this.chartReference.current.data.labels = labels;
+    this.chartReference.current.data.datasets[0].data = data;
+    this.chartReference.current.update();
+  }
+
+  componentDidMount() {
+    this.callApi();
+  }
+
+  render() {
+    return (
+      <h1>
+        Stock Price Analysis Dashboard1 ☘️ MNT
+        <p></p>
+        <Line
+          ref={this.chartReference}
+          id="main"
+          options={options}
+        />
+      </h1>
+
+    )
+  }
 }
 
 export default App
