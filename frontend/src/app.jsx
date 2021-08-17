2bc9a791181cbf1f3c41a38f547d35c24617f9ea
Thai Nhat Minh <t.n.minh1997@gmail.com>
2021-08-17 15:47:08 +0700

    + Add price of change graph to frontend

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -29,6 +29,7 @@ class App extends Component {
     super(props)
     this.state = { model: "LSTM" }
     this.chartReference = React.createRef();
+    this.chartReferencePoc = React.createRef();
   }
 
   callApi(model) {
@@ -37,10 +38,11 @@ class App extends Component {
       crossDomain: true,
 
     }).then(response => response.json())
-    .then(data => this.updateData(data)).catch(error => console.error(error));
+      .then(data => this.updateData(data)).catch(error => console.error(error));
   }
 
   updateData(rawData) {
+    console.log(rawData);
     var datasets = [{
       label: "Price predictions",
       borderColor: 'rgb(75, 192, 192)',
@@ -56,12 +58,28 @@ class App extends Component {
     var labels = [];
     indexs.forEach((element, index) => {
       labels.push(new Date(element));
-      datasets[0].data.push({x: new Date(element), y: predictions[index]});
-      datasets[1].data.push({x: new Date(element), y: closes[index]});
+      datasets[0].data.push({ x: new Date(element), y: predictions[index] });
+      datasets[1].data.push({ x: new Date(element), y: closes[index] });
     });
     this.chartReference.current.data.labels = labels;
     this.chartReference.current.data.datasets = datasets;
     this.chartReference.current.update();
+
+    var datasets_poc = [{
+      label: "Price of change",
+      borderColor: 'rgb(75, 192, 192)',
+      data: []
+    }];
+    var indexs_poc = rawData['IndexPoC'];
+    var pocs = rawData['poc'];
+    var labels_poc = [];
+    indexs_poc.forEach((element, index) => {
+      labels_poc.push(new Date(element));
+      datasets_poc[0].data.push({ x: new Date(element), y: pocs[index] });
+    });
+    this.chartReferencePoc.current.data.labels = labels_poc;
+    this.chartReferencePoc.current.data.datasets = datasets_poc;
+    this.chartReferencePoc.current.update();
   }
 
   componentDidMount() {
@@ -73,7 +91,7 @@ class App extends Component {
     if (value != this.state.model) {
       this.callApi(value);
     }
-    this.setState({model: value});
+    this.setState({ model: value });
   }
 
   render() {
@@ -87,18 +105,18 @@ class App extends Component {
         >
           <Grid item><h1>Stock Price Analysis Dashboard1 ☘️</h1></Grid>
           <Grid item><InputLabel id="select-model">Model</InputLabel>
-          <Select
-            labelId="select-model"
-            id="select-model"
-            value={this.state.model}
-            onChange={(event) => this.changeModel(event.target.value)}
-          >
-            <MenuItem value={'XGBoost'}>XGBoost</MenuItem>
-            <MenuItem value={'RNN'}>RNN</MenuItem>
-            <MenuItem value={'LSTM'}>LSTM</MenuItem>
-            <MenuItem value={'SVR'}>SVR</MenuItem>
-            <MenuItem value={'Linear'}>Linear</MenuItem>
-          </Select>
+            <Select
+              labelId="select-model"
+              id="select-model"
+              value={this.state.model}
+              onChange={(event) => this.changeModel(event.target.value)}
+            >
+              <MenuItem value={'XGBoost'}>XGBoost</MenuItem>
+              <MenuItem value={'RNN'}>RNN</MenuItem>
+              <MenuItem value={'LSTM'}>LSTM</MenuItem>
+              <MenuItem value={'SVR'}>SVR</MenuItem>
+              <MenuItem value={'Linear'}>Linear</MenuItem>
+            </Select>
           </Grid>
         </Grid>
         <Line
@@ -106,6 +124,20 @@ class App extends Component {
           id="main"
           options={options}
         />
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item><h1>Price of Change</h1></Grid>
+          <Line
+            ref={this.chartReferencePoc}
+            id="poc"
+            options={options}
+          />
+
+        </Grid>
       </div>
 
     )
